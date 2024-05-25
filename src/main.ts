fbc03a00ecc51bc06dc4ed68b041558463002668
Lanz Tristan Vasquez <lanzvasquez24@gmail.com>
2024-05-25 11:43:34 +0800

    word dictionary downloaded

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("Test");
+interface Words {
+	[key: string]: number;
+}
+const words = (() => {
+	let data = "";
+	return async () => {
+		if (!data) {
+			const response = await fetch("./words.json");
+			data = await response.json();
+		}
+
+		return data;
+	};
+})();
