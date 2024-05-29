bb17533e52c1666252ea86b63216d8a59db63250
Lanz Tristan Vasquez <lanzvasquez24@gmail.com>
2024-05-29 15:43:29 +0800

    - crosswords algorithm written - fixed a ton of bugs - fixed styling on tables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 // TODO: Use bigger but dirtier english dataset > https://github.com/harshnative/words-dataset
 // TODO: Remove weird words in dataset
 
+const ERRORS = {
+	WORD_LENGTH: new Error("No word with requested length"),
+	CHAR_MATCH: new Error("Failed to find word with same letters"),
+};
 class Words {
 	private static list: string[];
 	private static listByLen = new Map<number, string[]>();
@@ -25,7 +29,7 @@ class Words {
 		if (!this.list) await this.init();
 
 		const searchOn = isNaN(wordLen) ? this.list : this.listByLen.get(wordLen);
-		if (!searchOn) throw "No word with requested length";
+		if (!searchOn) throw ERRORS.WORD_LENGTH;
 
 		return new Array(cnt)
 			.fill(0)
@@ -55,7 +59,7 @@ class Words {
 			let tryCount = 50000; // scale tryCount against cnt?
 			while (avoid.includes(word) || !this.areCharacterSubsets(from, word!)) {
 				if (tryCount-- <= 0) {
-					throw new Error("Failed to find word with same letters");
+					throw ERRORS.CHAR_MATCH;
 				}
 
 				let index = Math.floor(Math.random() * searchRange);
@@ -101,18 +105,20 @@ const enum OVERLAP {
 	SIDE, // Letters on side
 	DIFF, // Non-matching letters
 }
+// TODO: fix extra space after stringify form
 class WordGrid {
 	public space: [string, number][][] = [];
-	public words = new Map<string, [number, number][]>(); // Track positions of words
+	public wordList: string[] = [];
+	public wordMap = new Map<string, [number, number][]>(); // Track positions of words
 	public usedArea = [Infinity, Infinity, -Infinity, -Infinity]; // Left, Top, Right, Bottom
 	private outdatedArea = false;
 
 	public add(word: string, x: number, y: number, isHorz = true): boolean {
-		if (this.words.has(word)) return false;
+		if (this.wordMap.has(word)) return false;
 
 		// Store actual used grid space
-		if (x < this.usedArea[0]) this.usedArea[0] = x;
-		if (y < this.usedArea[1]) this.usedArea[1] = y;
+		if (x <= this.usedArea[0]) this.usedArea[0] = x;
+		if (y <= this.usedArea[1]) this.usedArea[1] = y;
 
 		const posArr: [number, number][] = [];
 		for (let i = 0; i < word.length; i++) {
@@ -127,17 +133,18 @@ class WordGrid {
 			else y++;
 		}
 
-		if (x > this.usedArea[2]) this.usedArea[2] = x;
-		if (y > this.usedArea[3]) this.usedArea[3] = y;
+		if (x >= this.usedArea[2]) this.usedArea[2] = x + 1;
+		if (y >= this.usedArea[3]) this.usedArea[3] = y + 1;
 
-		// Save word positions
-		this.words.set(word, posArr);
+		// Save word
+		this.wordMap.set(word, posArr);
+		this.wordList.push(word);
 		return true;
 	}
 
 	public del(word: string): boolean {
-		if (this.words.has(word)) return false;
-		const posArr = this.words.get(word)!;
+		if (this.wordMap.has(word)) return false;
+		const posArr = this.wordMap.get(word)!;
 		for (let i = 0; i < word.length; i++) {
 			// If letter cnt < 1, delete letter
 			const [x, y] = posArr[i];
@@ -156,44 +163,54 @@ class WordGrid {
 				delete this.space[x][y];
 			}
 		}
-		this.words.delete(word);
 
+		// Delete word from lists
+		const wordIndex = this.wordList.findIndex((w) => w === word);
+		const lastIndex = this.wordList.length - 1;
+		[this.wordList[wordIndex], this.wordList[lastIndex]] = [
+			this.wordList[lastIndex],
+			this.wordList[wordIndex],
+		];
+		this.wordList.length--;
+		this.wordMap.delete(word);
 		return true;
 	}
 
 	public overlap(word: string, x: number, y: number, isHorz = true): OVERLAP {
-		let letterOverlap = false;
+		let overlap: undefined | [number, number];
 		for (let i = 0; i < word.length; i++) {
 			// Check if letter at x,y exists ...
-			let letterMatch = false;
 			if (this.space[x]?.[y]) {
-				letterOverlap = true;
 				// ... AND differs from current letter
-				console.log(x, y, this.space[x][y][0]);
 				if (this.space[x][y][0] !== word[i]) return OVERLAP.DIFF;
-				else letterMatch = true;
+				else overlap = [x, y];
 			}
 
 			// Check top and bottom
-			const areaCheck: [number, number][] = [];
-			if (isHorz || i === 0) areaCheck.push([x, y - 1]);
-			if (isHorz || i === word.length - 1) areaCheck.push([x, y + 1]);
-			if (!isHorz || i === 0) areaCheck.push([x - 1, y]);
-			if (!isHorz || i === word.length - 1) areaCheck.push([x + 1, y]);
-			for (const [ax, ay] of areaCheck) {
-				if (this.space[ax]?.[ay]) return OVERLAP.SIDE;
-			}
+			const surroundCheck: [number, number][] = [];
+			const isMatched = JSON.stringify(overlap) === `[${x},${y}]`;
+			const last = word.length - 1;
+			if ((isHorz && (isMatched || i === 0)) || (!isHorz && !isMatched))
+				surroundCheck.push([x - 1, y]);
+			if ((isHorz && (isMatched || i === last)) || (!isHorz && !isMatched))
+				surroundCheck.push([x + 1, y]);
+			if ((!isHorz && (isMatched || i === 0)) || (isHorz && !isMatched))
+				surroundCheck.push([x, y - 1]);
+			if ((!isHorz && (isMatched || i === last)) || (isHorz && !isMatched))
+				surroundCheck.push([x, y + 1]);
+			if (surroundCheck.some(([ax, ay]) => this.space[ax]?.[ay]))
+				return OVERLAP.SIDE;
 
 			// Move along word
 			if (isHorz) x++;
 			else y++;
 		}
-		return letterOverlap ? OVERLAP.MATCH : OVERLAP.NONE;
+		return overlap ? OVERLAP.MATCH : OVERLAP.NONE;
 	}
 
 	public stringify(): string {
 		if (this.outdatedArea) {
-			this.usedArea = [...this.words.values()].reduce(
+			this.usedArea = [...this.wordMap.values()].reduce(
 				([lx, ly, hx, hy], list) => {
 					return [
 						Math.min(lx, list[0][0]),
@@ -204,42 +221,83 @@ class WordGrid {
 				},
 				[Infinity, Infinity, -Infinity, -Infinity]
 			);
+			this.usedArea[2]++;
+			this.usedArea[3]++;
 			this.outdatedArea = false;
 		}
 
 		let str = "";
-		for (let y = this.usedArea[1]; y + 1 <= this.usedArea[3]; y++) {
-			for (let x = this.usedArea[0]; x + 1 <= this.usedArea[2]; x++) {
-				str += this.space[x]?.[y]?.[0] ?? " ";
+		for (let y = this.usedArea[1]; y < this.usedArea[3]; y++) {
+			for (let x = this.usedArea[0]; x < this.usedArea[2]; x++) {
+				const letter = this.space[x]?.[y]?.[0] ?? " ";
+				str += letter;
 			}
-			if (y <= this.usedArea[3]) str += "\n";
+			if (y < this.usedArea[3]) str += "\n";
 		}
 		return str;
 	}
 }
 
-async function main() {
-	const [parent] = await Words.getRandom(1, 6);
-	const [child] = await Words.getWordWithChars(1, parent);
-	const [grandchild] = await Words.getWordWithChars(1, child);
-	console.log(parent, child, grandchild);
+function randBool(): boolean {
+	return Math.random() < 0.5 ? false : true;
+}
+function randFloat(max: number, min = 0): number {
+	return Math.random() * (max - min) + min;
+}
+function randInt(max: number, min = 0): number {
+	return Math.floor(randFloat(min, max));
+}
+function shuffle<T>(arr: T[]): T[] {
+	let i = arr.length;
+	while (--i > 0) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const tmp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = tmp;
+	}
+	return arr;
+}
 
+async function main() {
 	const grid = new WordGrid();
-	grid.add(parent, 0, 0);
-	console.log(
-		grid.overlap(
-			child,
-			parent.split("").findIndex((char) => char === child[0]),
-			0,
-			false
-		)
-	);
-	grid.add(
-		child,
-		parent.split("").findIndex((char) => char === child[0]),
-		0,
-		false
-	);
+
+	const [root] = await Words.getRandom(randInt(15, 6));
+	grid.add(root, 0, 0, randBool());
+	for (let _ = 0; _ < 100; _++) {
+		try {
+			const branch = grid.wordList[randInt(grid.wordList.length)];
+			const branchPos = grid.wordMap.get(branch)!;
+			const children = await Words.getWordWithChars(5, root);
+			const childIsHorz = branchPos[0][0] === branchPos[1][0];
+
+			placeWord: for (const child of children) {
+				const randomIndices = shuffle(
+					Array(child.length)
+						.fill(0)
+						.map((_, i) => i)
+				);
+
+				for (const childIndex of randomIndices) {
+					const branchIndex = branch.indexOf(child[childIndex]);
+					if (branchIndex === -1) continue;
+
+					let [x, y] = branchPos[branchIndex];
+					if (childIsHorz) x -= childIndex;
+					else y -= childIndex;
+
+					if (
+						!grid.wordMap.has(child) &&
+						grid.overlap(child, x, y, childIsHorz) <= OVERLAP.MATCH
+					) {
+						grid.add(child, x, y, childIsHorz);
+						break placeWord;
+					}
+				}
+			}
+		} catch (err) {
+			if (err !== ERRORS.CHAR_MATCH) throw err;
+		}
+	}
 
 	const gridStr = grid.stringify();
 	const table = document.createElement("table");
@@ -251,12 +309,12 @@ async function main() {
 		}
 		if (char !== "\n") {
 			const charEl = document.createElement("td");
-			if (char !== " ") charEl.textContent = char;
+			// if (char !== " ") charEl.textContent = char;
+			charEl.textContent = char === " " ? "" : char;
 			curRow.appendChild(charEl);
 		}
 	}
 	document.body.appendChild(table);
-	console.log(gridStr);
 }
 
 main();
